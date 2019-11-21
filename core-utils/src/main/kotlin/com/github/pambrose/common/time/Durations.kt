f321e66f8dca5c798193f688b0150a285c7602eb
Paul Ambrose <pambrose@mac.com>
2019-11-20 19:50:02 -0800

    Add days to duration format

--- core-utils/src/main/kotlin/com/github/pambrose/common/time/Durations.kt
+++ core-utils/src/main/kotlin/com/github/pambrose/common/time/Durations.kt
@@ -40,16 +40,18 @@ fun timeUnitToDuration(value: Long, timeUnit: TimeUnit): Duration =
 
 fun Duration.format(includeMillis: Boolean = false): String {
   val diff = toLongMilliseconds()
-  val hr = MILLISECONDS.toHours(diff)
+  val day = MILLISECONDS.toDays(diff)
+  val dayMillis = DAYS.toMillis(day)
+  val hr = MILLISECONDS.toHours(diff - dayMillis)
   val hrMillis = HOURS.toMillis(hr)
-  val min = MILLISECONDS.toMinutes(diff - hrMillis)
+  val min = MILLISECONDS.toMinutes(diff - dayMillis - hrMillis)
   val minMillis = MINUTES.toMillis(min)
-  val sec = MILLISECONDS.toSeconds(diff - hrMillis - minMillis)
+  val sec = MILLISECONDS.toSeconds(diff - dayMillis - hrMillis - minMillis)
   val secMillis = SECONDS.toMillis(sec)
-  val ms = MILLISECONDS.toMillis(diff - hrMillis - minMillis - secMillis)
+  val ms = MILLISECONDS.toMillis(diff - dayMillis - hrMillis - minMillis - secMillis)
 
   return if (includeMillis)
-    java.lang.String.format("%02d:%02d:%02d.%03d", hr, min, sec, ms)
+    String.format("%d:%02d:%02d:%02d.%03d", day, hr, min, sec, ms)
   else
-    java.lang.String.format("%02d:%02d:%02d", hr, min, sec)
+    String.format("%d:%02d:%02d:%02d", day, hr, min, sec)
 }
