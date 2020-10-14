6cbf25de1cae6668a856a1f7902de453e0fb3e03
pambrose <pambrose@mac.com>
2020-10-13 22:05:30 -0700

    Add Atomic.kt

---
+++ core-utils/src/main/kotlin/com/github/pambrose/common/concurrent/Atomic.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2020 Paul Ambrose (pambrose@mac.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.pambrose.common.concurrent
+
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+// See: https://medium.com/swlh/kotlin-for-lunch-atomic-t-261351048fad
+
+class Atomic<T>(var value: T) : Mutex by Mutex() {
+  suspend inline fun setWithLock(owner: Any? = null, action: (T) -> T): T =
+    (this as Mutex).withLock(owner) { action(value).also { value = it } }
+
+  suspend inline fun <V> withLock(owner: Any? = null, action: T.() -> V): V =
+    (this as Mutex).withLock(owner) { value.action() }
+}
