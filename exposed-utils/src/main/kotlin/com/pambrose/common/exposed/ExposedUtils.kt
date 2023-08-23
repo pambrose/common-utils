7c6f36db8912434c7859b661ed4ffc4c5ad5a7d6
Paul Ambrose <pambrose@mac.com>
2023-08-23 10:16:22 -0700

    Update kotlin, exposed, grpc and logback jars

--- exposed-utils/src/main/kotlin/com/pambrose/common/exposed/ExposedUtils.kt
+++ exposed-utils/src/main/kotlin/com/pambrose/common/exposed/ExposedUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021 Paul Ambrose (pambrose@mac.com)
+ * Copyright © 2023 Paul Ambrose (pambrose@mac.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,16 @@ package com.pambrose.common.exposed
 
 import mu.two.KLogger
 import mu.two.KLogging
+import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.SqlLogger
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.statements.StatementContext
 import org.jetbrains.exposed.sql.statements.expandArgs
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.transactionManager
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.time.measureTimedValue
 
 object ExposedUtils : KLogging()
 
@@ -38,3 +43,31 @@ operator fun ResultRow.get(index: Int) = fieldIndex.filter { it.value == index }
 
 fun ResultRow.toRowString() =
   fieldIndex.values.map { this[it].toString() }.filter { it.isNotEmpty() }.joinToString(" - ")
+
+fun <T> readonlyTx(
+  db: Database? = null,
+  transactionIsolation: Int = db.transactionManager.defaultIsolationLevel,
+  statement: Transaction.() -> T
+): T =
+  transaction(
+    transactionIsolation,
+    true,
+    db,
+    statement
+  )
+
+inline fun <T> AtomicBoolean.criticalSection(block: () -> T) {
+  set(true)
+  try {
+    block()
+  } finally {
+    set(false)
+  }
+}
+
+fun <T> timedTransaction(block: () -> T) =
+  measureTimedValue {
+    transaction {
+      block()
+    }
+  }
