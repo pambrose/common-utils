a2ac4131aebc5dda5ea1c28aed9c7c71f940f866
pambrose <pambrose@mac.com>
2026-02-08 11:37:59 -0800

    Refactor IOExtensionsTests and JavaScript to use Int instead of Integer for type handling

--- script-utils-java/src/main/kotlin/com/github/pambrose/common/script/JavaScript.kt
+++ script-utils-java/src/main/kotlin/com/github/pambrose/common/script/JavaScript.kt
@@ -64,8 +64,10 @@ class JavaScript :
   private val KType.javaEquiv: String
     get() =
       when (this) {
-        typeOf<Int>() -> Integer::class.java.simpleName
-        typeOf<Int?>() -> Integer::class.java.simpleName
+//        typeOf<Int>() -> Integer::class.java.simpleName
+//        typeOf<Int?>() -> Integer::class.java.simpleName
+        typeOf<Int>() -> Int::class.javaObjectType.simpleName
+        typeOf<Int?>() -> Int::class.javaObjectType.simpleName
         else -> this.toString().removePrefix("kotlin.").replace("?", "")
       }
 
